b86ca0aa9e9980ec67ade85d33b3477da1e85e1e
Raul Diaz <raul.15.94@hotmail.com>
2023-02-02 18:01:55 +0100

    Add Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 export default function Navbar(){
     return (
-        <div id="Navbar" className="flex justify-end flex-row">
-            <div className="p-8"><a href="#">About me</a></div>
-            <div className="p-8">Contact</div>
-            <div className="p-8">Projects</div>
-        </div>
+        <>
+            <div id="Navbar" className="flex justify-end flex-row bg-red-400">
+                <div className="p-8 bg-lime-700"><a className="hover:bg-fuchsia-100" href="#">About me</a></div>
+                <div className="p-8"><a className="hover:bg-fuchsia-100" href="#">Contact</a></div>
+                <div className="p-8"><a className="hover:bg-fuchsia-100" href="#">Menu</a></div>
+            </div>
+        </>
     )
 }       

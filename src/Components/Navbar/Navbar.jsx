357ee940a0d5fae60451543b2c184bf5c2182d2a
Raul Diaz <raul.15.94@hotmail.com>
2023-02-02 17:50:42 +0100

    Create React Vite

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <div id="Navbar" className="flex justify-end flex-row">
+            <div className="p-8"><a href="#">About me</a></div>
+            <div className="p-8">Contact</div>
+            <div className="p-8">Projects</div>
+        </div>
+    )
+}       

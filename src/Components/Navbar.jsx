a4a06f526be75e0f1a6538f265866f5fc3a161c8
Raul Diaz <raul.15.94@hotmail.com>
2023-02-02 22:26:19 +0100

    Add heart

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return (
         <>
-            <div id="Navbar" className="m-auto flex items-center w-11/12 justify-end flex-row space-x-4">
+            <div id="Navbar" className="m-auto mt-8 mb-2 flex items-center w-11/12 justify-end flex-row space-x-4">
                 <div className="p-4 flex items-center flex-auto"><img className="max-h-16 rounded-full" src="src/assets/photo_perfil.jpg"></img></div>
                 <div className="p-8  "><a className="text-2xl font-black text-gray-200 font-roboto" href="#">About me</a></div>
                 <div className="p-8  "><a className="text-2xl font-black text-gray-200 font-roboto" href="#">Contact</a></div>
--- src/Components/Vange.jsx
+++ src/Components/Vange.jsx
@@ -1,8 +1,11 @@
+// import HeartSVG from "src/assets/heart.svg";
+
 export default function Vange(){
     return (
         <>
-        <div className="w-full justify-center flex">
-            <span className="text-9xl text-center">TE QUIERO!!</span>        
+        <div className="place-items-center h-96 animate-bounce w-full justify-center flex m-1 flex flex-wrap" >
+            <span className="text-9xl text-center">TE QUIERO!!</span>
+            <img src="src/assets/heart.svg" alt="SVG" className="w-14" />
         </div>
         </>
     )
